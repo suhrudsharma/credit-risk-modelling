78aae85c6c19c4ebcc2f1467a323be3a12876987
suhrudsharma <suhrudsharma@gmail.com>
2026-04-17 16:10:11 +0530

    updated to use as PWA

---
+++ frontend/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'creditrisk-pwa-v1';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(
+          () => console.log('Fetch failed, likely offline.')
+        );
+      })
+  );
+});
